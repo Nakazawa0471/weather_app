1714c56b4a36b5477fc1d8936e4d0524d982dac0
Masafumi Nakazawa <masafumi.nakazawa0471@gmail.com>
2025-09-08 03:37:11 +0900

    Update app.py to use dynamic port for Render

--- app.py
+++ app.py
@@ -42,4 +42,5 @@ def index():
     return render_template("index.html", city=city, weather_data=weather_data, icon=icon)
 
 if __name__== "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
